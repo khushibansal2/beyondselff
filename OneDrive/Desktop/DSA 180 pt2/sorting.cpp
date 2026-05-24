ca49331cd3fd898d5b1fa2642b7e427cbfa24cfe
Khushi Bansal <khushibansal4207@gmail.com>
2026-05-24 19:56:27 +0530

    Restore unrelated files accidentally deleted (CLAUDE.md and OneDrive examples)

---
+++ OneDrive/Desktop/DSA 180 pt2/sorting.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int arr[5] = {10 , 4, 3, 4, 6};
+
+    for(int i=0; i<4; i++) {
+
+        int index = i;
+        for(int j = i+1; j<5 ; j++){
+
+            if (arr[j]<arr[index])
+            index = j;
+        }
+
+        swap(arr[i],arr[index]);
+    }
+
+    for (int i=0; i<5; i++) {
+        cout<<arr[i]<<" ";
+    }
+
+
+}
+
+
